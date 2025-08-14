6b70fd1c96583d2c9c4ea0350469095c8c7c1b65
Azim <yoba.number.one@gmail.com>
2025-08-15 01:45:38 +0300

    test(contract): adds smart-contract deploy tests

--- src/contract/test/DonateContract.js
+++ src/contract/test/DonateContract.js
@@ -1,14 +1,15 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("DonateContract", function () {
     async function deployDonateFixture() {
-        const [owner, otherAccount] = await ethers.getSigners();
+        const [owner, otherAccount, thirdAccount] = await ethers.getSigners();
 
         const DonateContract = await ethers.getContractFactory("DonateContract");
-        const donateContract = await DonateContract.deploy({ totalDonations, addressToDonate });
+        const donateContract = await DonateContract.deploy();
 
-        return { donateContract, owner, otherAccount, totalDonations, addressToDonate };
+        return { donateContract, owner, otherAccount, thirdAccount };
     }
 
     describe("Deployment", function () {
@@ -20,13 +21,31 @@ describe("DonateContract", function () {
         it("Should set the correct totalDonations", async function () {
             const { donateContract, otherAccount } = await loadFixture(deployDonateFixture);
 
-            expect(await donateContract.totalDonations()).to.equal(0);
+            expect(await donateContract.totalDonations()).to.equal(0n);
 
-            await donateContract.connect(otherAccount).donate("name", "msg", { value: ethers.parseEther("1") });
-            expect(await donateContract.totalDonations()).to.equal(ethers.parseEther("1"));
+            const donationAmount = ethers.parseEther("1");
+            await donateContract.connect(otherAccount).donate("name", "msg", { value: donationAmount });
+
+            expect(await donateContract.totalDonations()).to.equal(donationAmount);
         })
-        it("Should set the correct addressToDonate", async function () {
-            const { donateContract, otherAccount } = await loadFixture(deployDonateFixture);
+        it("Should correctly track donations for a specific address", async function () {
+            const { donateContract, otherAccount, thirdAccount } = await loadFixture(deployDonateFixture);
+
+            expect(await donateContract.addressToDonate(otherAccount.address).to.equal(0n));
+
+            const donationAmount1 = ethers.parseEther("1");
+            await donateContract.connect(otherAccount).donate("name1", "msg1", { value: donationAmount1 });
+            expect(await donateContract.addressToDonate(otherAccount.address).to.equal(donationAmount1));
+
+            const donationAmount2 = ethers.parseEther("0.5");
+            await donateContract.connect(otherAccount).donate("name2", "msg2", { value: donationAmount2 });
+            expect(await donateContract.addressToDonate(otherAccount.address).to.equal(donationAmount1 + donationAmount2));
+
+            expect(await donateContract.addressToDonate(thirdAccount.address).to.equal(0n));
         })
+    })
+
+    describe("Events", function () {
+        
     })
 })
